5ab40d89fdc5c07bcfec82939d0e75da6fbb625e
daniel iya <iyadaniel51@gmail.com>
2022-05-13 04:46:31 +0100

    modif

--- 4.cv_framework_Localstorage-ict-202/atouts/js/script.js
+++ 4.cv_framework_Localstorage-ict-202/atouts/js/script.js
@@ -61,11 +61,8 @@ document.addEventListener("DOMContentLoaded", ()=>{
             "li3_competences" : "Maintenance des ordinateurs",
             "li4_competences" : "Informatisation des systêmes",
             "li5_competences" : "Prestation de service",
-            "li6_competences" : "Infographie multiméia"
-        },
-        {
-            "h3_logiciels" : "LOGICIELS ET SE",
-            "li1_logiciels" : "Abode photoshop",
+            "li6_competences" : "Infographie multiméia",
+			"li1_logiciels" : "Abode photoshop",
             "li2_logiciels" : "Affinity Designer",
             "li3_logiciels" : "Corel draw",
             "li4_logiciels" : "GitHub Desktop",
@@ -82,45 +79,43 @@ document.addEventListener("DOMContentLoaded", ()=>{
 }
 
     var set = JSON.stringify(cv);
-        localStorage.setItem("diplomes",set);
+        localStorage.setItem("cv",set);
 
-    var getCV = localStorage.getItem("diplomes");
+    var getCV = localStorage.getItem("cv");
     var get = JSON.parse(getCV);
     
     if(get != null){
     //diplome
-        document.getElementById("h3_diplome").innerHTML += `${get[0].h3_diplomes}`;
-        document.getElementById("h41_diplome").innerHTML += `${get[0].h41_diplomes}`;
-        document.getElementById("li1_diplome").innerHTML += `${get[0].li1_diplomes}`;
-        document.getElementById("li2_diplome").innerHTML += `${get[0].li2_diplomes}`;
-        document.getElementById("li3_diplome").innerHTML += `${get[0].li3_diplomes}`;
-        document.getElementById("h42_diplome").innerHTML += `${get[0].h42_diplomes}`;
-        document.getElementById("li4_diplome").innerHTML += `${get[0].li4_diplomes}`;
+        document.getElementById("h3_diplome").innerHTML = `${get[0].h3_diplomes}`;
+        document.getElementById("h41_diplome").innerHTML = `${get[0].h41_diplomes}`;
+        document.getElementById("li1_diplome").innerHTML = `${get[0].li1_diplomes}`;
+        document.getElementById("li2_diplome").innerHTML = `${get[0].li2_diplomes}`;
+        document.getElementById("li3_diplome").innerHTML = `${get[0].li3_diplomes}`;
+        document.getElementById("h42_diplome").innerHTML = `${get[0].h42_diplomes}`;
+        document.getElementById("li4_diplome").innerHTML = `${get[0].li4_diplomes}`;
     //formation
-        document.getElementById("h3_formation").innerHTML += `${get[1].h3_formations}`;
-        document.getElementById("h4_formation").innerHTML += `${get[1].h4_formations}`;
-        document.getElementById("p_formation").innerHTML += `${get[1].p_formations}`;
+        document.getElementById("h3_formation").innerHTML = `${get[1].h3_formations}`;
+        document.getElementById("h4_formation").innerHTML = `${get[1].h4_formations}`;
+        document.getElementById("p_formation").innerHTML = `${get[1].p_formations}`;
     //experience
-        document.getElementById("h3_experience").innerHTML += `${get[2].h3_experiences}`;
-        document.getElementById("h4_experience").innerHTML += `${get[2].h4_experiences}`;
-        document.getElementById("p1_experience").innerHTML += `${get[2].p1_experiences}`;
-        document.getElementById("p2_experience").innerHTML += `${get[2].p2_experiences}`;
+        document.getElementById("h3_experience").innerHTML = `${get[2].h3_experiences}`;
+        document.getElementById("h4_experience").innerHTML = `${get[2].h4_experiences}`;
+        document.getElementById("p1_experience").innerHTML = `${get[2].p1_experiences}`;
+        document.getElementById("p2_experience").innerHTML = `${get[2].p2_experiences}`;
     //competence
-        document.getElementById("h3_competence").innerHTML += `${get[3].h3_competences}`;
-        document.getElementById("li1_competence").innerHTML += `${get[3].li1_competences}`;
-        document.getElementById("li2_competence").innerHTML += `${get[3].li2_competences}`;
-        document.getElementById("li3_competence").innerHTML += `${get[3].li3_competences}`;
-        document.getElementById("li4_competence").innerHTML += `${get[3].li4_competences}`;
-        document.getElementById("li5_competence").innerHTML += `${get[3].li5_competences}`;
-        document.getElementById("li6_competence").innerHTML += `${get[3].li6_competences}`;
-    //LOGICIELS ET SE
-        document.getElementById("h3_logiciel").innerHTML += `${get[4].h3_logiciels}`;
-        document.getElementById("li1_logiciel").innerHTML += `${get[4].li1_logiciels}`;
-        document.getElementById("li2_logiciel").innerHTML += `${get[4].li2_logiciels}`;
-        document.getElementById("li3_logiciel").innerHTML += `${get[4].li3_logiciels}`;
-        document.getElementById("li4_logiciel").innerHTML += `${get[4].li4_logiciels}`;
-        document.getElementById("li5_logiciel").innerHTML += `${get[4].li5_logiciels}`;
-        document.getElementById("li6_logiciel").innerHTML += `${get[4].li6_logiciels}`;
+        document.getElementById("h3_competence").innerHTML = `${get[3].h3_competences}`;
+        document.getElementById("li1_competence").innerHTML = `${get[3].li1_competences}`;
+        document.getElementById("li2_competence").innerHTML = `${get[3].li2_competences}`;
+        document.getElementById("li3_competence").innerHTML = `${get[3].li3_competences}`;
+        document.getElementById("li4_competence").innerHTML = `${get[3].li4_competences}`;
+        document.getElementById("li5_competence").innerHTML = `${get[3].li5_competences}`;
+        document.getElementById("li6_competence").innerHTML = `${get[3].li6_competences}`;
+        document.getElementById("li1_logiciel").innerHTML = `${get[3].li1_logiciels}`;
+        document.getElementById("li2_logiciel").innerHTML = `${get[3].li2_logiciels}`;
+        document.getElementById("li3_logiciel").innerHTML = `${get[3].li3_logiciels}`;
+        document.getElementById("li4_logiciel").innerHTML = `${get[3].li4_logiciels}`;
+        document.getElementById("li5_logiciel").innerHTML = `${get[3].li5_logiciels}`;
+        document.getElementById("li6_logiciel").innerHTML = `${get[3].li6_logiciels}`;
     //copyright
         document.getElementById("copyright").innerHTML += `${get[5].copyright}`;
     //info personnels
